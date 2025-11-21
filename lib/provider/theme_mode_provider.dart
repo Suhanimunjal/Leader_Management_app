486839235af463735fe70055b35b019ea7bb67f7
Suhanimunjal <159304951+Suhanimunjal@users.noreply.github.com>
2025-11-21 15:03:55 +0530

    Update theme_mode_provider.dart

--- lib/provider/theme_mode_provider.dart
+++ lib/provider/theme_mode_provider.dart
@@ -2,5 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
-/// Holds the current [ThemeMode] for the app.
 final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.light);
